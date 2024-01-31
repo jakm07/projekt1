de04e6f52e21978ccac34a44a10b125462e21118
Michaela Jakubíková <jakubi.mich@gmail.com>
2024-01-31 20:20:55 +0100

    oprava kodu

--- script02.js
+++ script02.js
@@ -6,8 +6,8 @@ let prijem = pocetPredstaveni*(cenaListku*navstenici);
 
 document.write("Měsíční příjem divadla: " + prijem + " eur" + "<br>");
 
-let studenti = (0.4*prijem)*0.65
-let nestudenti = 0.6*prijem
-let novyPrijem = toString(studenti) + toString(novyPrijem)
+let studenti = (0.4*prijem)*0.65;
+let nestudenti = 0.6*prijem;
+let novyPrijem = toString(studenti) + toString(novyPrijem);
 
-document.write("Měsíční příjem se slevou: " + novyPrijem + " eur" + "<br>")
+document.write("Měsíční příjem se slevou: " + novyPrijem + " eur" + "<br>");
