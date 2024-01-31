5e86fffba1be796498b24cd336c65587e5fe7840
Michaela Jakubíková <jakubi.mich@gmail.com>
2024-01-31 20:14:51 +0100

    vytvoreni ukolu s nazvem Delka filmu

---
+++ script02.js
@@ -0,0 +1,13 @@
+let cenaListku = 12;
+let pocetPredstaveni = 15;
+let navstenici = 174;
+
+let prijem = pocetPredstaveni*(cenaListku*navstenici);
+
+document.write("Měsíční příjem divadla: " + prijem + " eur" + "<br>");
+
+let studenti = (0.4*prijem)*0.65
+let nestudenti = 0.6*prijem
+let novyPrijem = toString(studenti) + toString(novyPrijem)
+
+document.write("Měsíční příjem se slevou: " + novyPrijem + " eur" + "<br>")
